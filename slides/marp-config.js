b3b72221d6d48db4e79e53f7380776742813352a
Josh Garverick <josh.garverick@gmail.com>
2023-03-31 17:44:37 -0400

    Update actions and config

--- slides/marp-config.js
+++ slides/marp-config.js
@@ -1,11 +1,10 @@
-const { Marpit, ThemeSet } = require('@marp-team/marpit');
+const { Marpit } = require('@marp-team/marpit');
 const fs = require('@supercharge/fs');
 
 const marpit = new Marpit({
     markdown: {
-        html: true
+        enableHtml: true
     }
 });
-
-fs.content("./themes/sunset.css").then((value) => {marpit.themeSet.add(value)});
-fs.content("./themes/midnight.css").then((value) => {marpit.themeSet.add(value)});
+fs.content("./themes/sunset.css").then((value) => { marpit.themeSet.add(value) });
+fs.content("./themes/midnight.css").then((value) => { marpit.themeSet.add(value) });
